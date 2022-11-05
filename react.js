d7cbf7b7a8c8a4d5ffb4a1f7a32f2154bf2981f9
sharafath ali <sharafathaliofficial@gmail.com>
2022-11-06 01:06:59 +0530

    second

--- react.js
+++ react.js
@@ -3,7 +3,9 @@ ReactDOM.render(<h3>this is h3 using ReactDOM</h3>,document.getElementById("h3")
 ReactDOM.render(<ul><li>this</li><li>is</li><li>list</li>
 <li>using react</li></ul>,document.getElementById("ul"))
 function Dif(){
-   return(<h1>h1 in first function</h1>)
+   return(
+         <h1>h1 in first function</h1>
+         )
 }
 function Navb() {
     return (
@@ -11,8 +13,9 @@ function Navb() {
     <h1>h1 in second function</h1>
     <img src="fullsnap.png" width="30" height="30" alt=""/>
     <button type="button" class="btn btn-primary">Primary</button>
-    </div>
     
+    </div>
+
     )
 }
 
@@ -23,3 +26,15 @@ ReactDOM.render(
     </div>,
     document.getElementById("nav")
 )
+*/challenge*/
+const nav=(
+    <nav>
+    <h1>OWNIT</h1>
+    <ul>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+    </ul>
+    </nav>
+)
+ReactDOM.render(nav,document.getElementById("cnav"))
