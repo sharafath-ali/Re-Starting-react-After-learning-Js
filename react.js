f6deb5fba1db4312bd7a799fdc90cf59fc8ec9e0
sharafath ali <sharafathaliofficial@gmail.com>
2022-11-07 00:03:29 +0530

    f

--- react.js
+++ react.js
@@ -26,6 +26,8 @@ ReactDOM.render(
     </div>,
     document.getElementById("nav")
 )
+
+
 */challenge*/
 const nav=(
     <nav>
@@ -38,3 +40,95 @@ const nav=(
     </nav>
 )
 ReactDOM.render(nav,document.getElementById("cnav"))
+
+
+*/challenge =jsx assing to a variable and use an append()*/
+const a=(
+    <div>
+        <h1>jsx using append()</h1>
+        <h2>we cant do that with append</h2>
+    </div>
+)
+document.getElementById("append").append(JSON.stringify(a));
+ReactDOM.render(a,document.getElementById("append"))
+
+
+*/challenge*/
+const b=(
+    <div>
+      <img src="imgreact.png" width="70px" />
+      <h1>Fun facts about React</h1>
+      <ol>
+        <li>2013</li>
+        <li>100k stars on github</li>
+        <li>maintained by meta</li>
+      </ol>
+
+
+    </div>
+)
+ReactDOM.render(b,document.getElementById("react"))
+*/challenge console.log(b)=a javascript Object.react element that describe what react should add to thr real dom for us */
+*/challenge*/
+
+function Time(){
+    return (
+        <div>
+           <Heade />
+         <header>
+            <nav>
+                <img src="imgreact.png" width="70px"/>
+            </nav>
+         </header>
+         <h1>custom page component</h1>
+         <ol>
+            <li>easy</li>
+            <li>smartway</li>
+         </ol>
+         
+        <footer>
+            <small>Ⓒ 2022 sharafath development .All rights reserved</small>
+        </footer>
+        </div>
+    )
+}
+ReactDOM.render(<Time /> ,document.getElementById("custom"))
+*/challenge =react component =function that returns react elements (ui)*/
+function Heade(){
+    return(
+        <div>
+        <header>
+            <nav>
+                <img src="imgreact.png" width="70px"/>
+            </nav>
+         </header>
+         </div>
+    )
+}
+function Ol(){
+    return(
+          <div>
+          <ol>
+           <li>easy</li>
+           <li>smartway</li>
+          </ol>
+          </div>
+          )
+}
+function Foot(){
+    return(
+        <div>
+        <footer>
+        <small>Ⓒ 2022 sharafath development .All rights reserved</small>
+        </footer>
+        </div>
+    )
+}
+function Comp(){
+    return(<div>
+    <Heade />
+    <Ol />
+    <Foot />
+    </div>)
+}
+ReactDOM.render(<Comp />,document.getElementById("comp"))
