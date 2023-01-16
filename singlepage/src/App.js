2acc0492b7d2147e463af92603ab2fd191d19ada
sharafath ali <sharafathaliofficial@gmail.com>
2023-01-17 01:05:40 +0530

    o

---
+++ singlepage/src/App.js
@@ -0,0 +1,15 @@
+import { useState } from "react"
+function App() {
+  const [count,setcount]=useState(0)
+  var Addcount=()=>{
+    setcount(count+1)
+    
+  }
+  return (
+    <div>
+    <button onClick={Addcount} >ADD</button>
+    <h1>count: {count}</h1>
+    </div>
+  );
+}
+export default App;
